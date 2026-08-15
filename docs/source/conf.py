fb6013dfd677bbc50c7db81b957c411e95cd4fa1
Rohit Goswami <rgoswami@ieee.org>
2026-08-15 13:08:42 -0500

    REL: Bump version to v0.3.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ import shutil
 project = "asv_runner"
 copyright = "2023--present, asv Developers"
 author = "asv Developers"
-release = "0.3.0"
+release = "0.3.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
